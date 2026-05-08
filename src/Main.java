7f5172707d689afd330a8ec9f148b13e85eb59b8
Franze Pintal <fpintal@charlotte.edu>
2026-05-08 14:04:21 -0400

    Refactor Main class to launch OutfitBuilderView; remove unused service and user initialization code

--- src/Main.java
+++ src/Main.java
@@ -1,98 +1,11 @@
 package src;
 
-import src.models.*;
-import src.services.*;
+import javafx.application.Application;
+import src.views.OutfitBuilderView;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        // Create services
-        WardrobeService wardrobeService = new WardrobeService();
-        OutfitService outfitService = new OutfitService();
-        SaveService saveService = new SaveService();
-        ImageService imageService = new ImageService();
-
-        // Create user
-        User user = new User("1", "Keith");
-
-        // Create clothing items
-        Top hoodie = new Top(
-                "101",
-                "assets/tops/hoodie.png",
-                "Top",
-                "Black",
-                "Long Sleeve"
-        );
-
-        Top tshirt = new Top(
-        "102",
-        "assets/tops/tshirt.png",
-        "Top",
-        "White",
-        "Short Sleeve"
-);
-
-        Bottom jeans = new Bottom(
-                "201",
-                "assets/bottoms/jeans.png",
-                "Bottom",
-                "Blue",
-                "Baggy"
-        );
-
-        Shoe sneakers = new Shoe(
-                "301",
-                "assets/shoes/sneakers.png",
-                "Shoe",
-                "White",
-                "Sneakers"
-        );
-
-        // Test image validation
-        System.out.println("Is hoodie image valid? "
-                + imageService.isValidImageFile(hoodie.getImagePath()));
-
-        // Add clothing items using WardrobeService
-        wardrobeService.addItemToWardrobe(user, hoodie);
-        wardrobeService.addItemToWardrobe(user, tshirt);
-        wardrobeService.addItemToWardrobe(user, jeans);
-        wardrobeService.addItemToWardrobe(user, sneakers);
-        
-
-        // Create outfit using OutfitService
-        Outfit casualFit = outfitService.createOutfit("401", "Casual Fit");
-
-        outfitService.addItemToOutfit(casualFit, hoodie);
-        outfitService.addItemToOutfit(casualFit, jeans);
-        outfitService.addItemToOutfit(casualFit, sneakers);
-
-        // Save outfit using SaveService
-        saveService.saveOutfit(user, casualFit);
-
-        // Display results
-        System.out.println("\nUser Name: " + user.getName());
-        System.out.println("Number of Wardrobe Items: "
-                + user.getWardrobe().getClothingItems().size());
-
-        System.out.println("Number of Saved Outfits: "
-                + user.viewSavedOutfits().size());
-
-        System.out.println("\nWardrobe Items:");
-        wardrobeService.displayWardrobe(user);
-
-        System.out.println("\nOutfit Details:");
-        outfitService.displayOutfit(casualFit);
-
-        System.out.println("\n--- Switching Top ---");
-
-        outfitService.switchItemByCategory(casualFit, tshirt);
-
-        System.out.println("\nUpdated Outfit Details:");
-        outfitService.displayOutfit(casualFit);
-
-        // Saves user's wardrobe and outfits
-        saveService.saveWardrobe(user);
-        saveService.saveOutfits(user);
+        Application.launch(OutfitBuilderView.class, args);
     }
 }
--- src/views/OutfitBuilderView.java
+++ src/views/OutfitBuilderView.java
@@ -1,8 +1,99 @@
 package src.views;
 
-/**
- * Represents the view for building outfits.
- */
-public class OutfitBuilderView {
-    // Methods for rendering the outfit builder view will go here.
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class OutfitBuilderView extends Application {
+
+    private int topIndex = 0;
+    private int bottomIndex = 0;
+    private int shoeIndex = 0;
+
+    private final String[] topImages = {
+            "assets/tops/hoodie.png",
+            "assets/tops/tshirt.png"
+    };
+
+    private final String[] bottomImages = {
+            "assets/bottoms/jeans.png",
+            "assets/bottoms/cargos.png"
+    };
+
+    private final String[] shoeImages = {
+            "assets/shoes/sneakers.png",
+            "assets/shoes/boots.png"
+    };
+
+    @Override
+    public void start(Stage stage) {
+
+        Label title = new Label("Outfit Generator");
+
+        ImageView topView = createImageView(topImages[topIndex]);
+        ImageView bottomView = createImageView(bottomImages[bottomIndex]);
+        ImageView shoeView = createImageView(shoeImages[shoeIndex]);
+
+        Button switchTopButton = new Button("Switch Top");
+        Button switchBottomButton = new Button("Switch Bottom");
+        Button switchShoesButton = new Button("Switch Shoes");
+        Button saveButton = new Button("Save Outfit");
+
+        switchTopButton.setOnAction(e -> {
+            topIndex = (topIndex + 1) % topImages.length;
+            topView.setImage(new Image("file:" + topImages[topIndex]));
+        });
+
+        switchBottomButton.setOnAction(e -> {
+            bottomIndex = (bottomIndex + 1) % bottomImages.length;
+            bottomView.setImage(new Image("file:" + bottomImages[bottomIndex]));
+        });
+
+        switchShoesButton.setOnAction(e -> {
+            shoeIndex = (shoeIndex + 1) % shoeImages.length;
+            shoeView.setImage(new Image("file:" + shoeImages[shoeIndex]));
+        });
+
+        saveButton.setOnAction(e -> {
+            System.out.println("Outfit Saved!");
+            System.out.println("Top: " + topImages[topIndex]);
+            System.out.println("Bottom: " + bottomImages[bottomIndex]);
+            System.out.println("Shoes: " + shoeImages[shoeIndex]);
+        });
+
+        VBox root = new VBox(15);
+        root.setStyle("-fx-padding: 20; -fx-alignment: center;");
+
+        root.getChildren().addAll(
+                title,
+                topView,
+                bottomView,
+                shoeView,
+                switchTopButton,
+                switchBottomButton,
+                switchShoesButton,
+                saveButton
+        );
+
+        Scene scene = new Scene(root, 500, 700);
+
+        stage.setTitle("Outfit Generator");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private ImageView createImageView(String imagePath) {
+        Image image = new Image("file:" + imagePath);
+        ImageView imageView = new ImageView(image);
+
+        imageView.setFitWidth(200);
+        imageView.setPreserveRatio(true);
+
+        return imageView;
+    }
 }
